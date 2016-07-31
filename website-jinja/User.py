4fc2ef6128be98406e9092cd41a9270df42cd3f6
angelicali <liyunshuangelica@gmail.com>
2016-07-31 20:07:35 +0800

    removed files not used; improved settings page

---
+++ website-jinja/User.py
@@ -0,0 +1,114 @@
+from google.appengine.ext import ndb
+
+class Course(ndb.Model):
+	id = ndb.StringProperty(required=True)
+	subject = ndb.StringProperty()
+	number = ndb.IntegerProperty()
+	website = ndb.JsonProperty()	#{"www.coursesite.com":3,"www.subsite.com":1} records numbers of time people choose it
+	hw = ndb.JsonProperty()
+	calendar = ndb.JsonProperty()
+	grades = ndb.JsonProperty()
+	syllabus = ndb.JsonProperty()
+
+class User(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	userid = ndb.StringProperty(required=True)
+	email = ndb.StringProperty(required=True)
+	password = ndb.StringProperty(required=True)
+	courses = ndb.JsonProperty()
+	courseinfo = ndb.JsonProperty()	#overwrite default info; "empty" if user doesn't want it
+	joined_time = ndb.DateTimeProperty(auto_now_add=True)
+
+
+
+########### validation #####################
+import hashlib
+import random
+from string import letters
+SECRET = "kiwiNinja"
+
+def make_salt():
+	return ''.join(random.choice(letters) for _ in range(5))
+
+def pw_hash(id,pw,salt=None):
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name+pw+salt).hexdigest()
+	return '%s|%s'%(h,salt)
+
+def valid_pw(id,pw,HASH):
+	salt = HASH.split('|')[1]
+	if pw_hash(name,pw,salt) == HASH:
+		return True
+
+
+def text_hash(text):
+	h = hashlib.sha256(text+SECRET).hexdigest()
+	return '%s|%s'%(text,h)
+
+def valid_id(HASH):
+	id = HASH.split('|')[0]
+	if text_hash(id) == HASH:
+		return id
+
+########### validation #####################
+
+
+########## login/signup #####################
+def query_id(uid):
+	qry = User.query().filter(User.userid == uid)
+	return qry.fetch()
+
+def query_email(em):
+	qry = User.query().filter(User.email == em)
+	return qry.fetch()
+
+def query_key(key):
+	qry = User.query().filter(User.key == key)
+	return qry.fetch()
+
+def login(id,pw):
+	user = query_id(id)
+	#if valid_pw(id,pw):
+	if len(user)>0:
+		user = user[0]
+		if user.password == pw:
+			return user
+
+
+def signup(name,id,email,pw):
+	user = User(name=name,userid=id,email=email,password=pw)
+	user_key=user.put()
+	return user_key
+
+########## login/signup #####################
+
+########## course fucntions ##################
+def course_info_converter(text):
+	for line in text.splitlines():
+		try:
+			lst = line.split('\t')
+			crn = int(lst[0])
+			subject,number,section = lst[1].split()
+			name = lst[2]
+			prof = lst[-1]
+			credit = int(lst[4][0])
+		except:
+			pass
+
+def query_courseid(id):
+	qry = Course.query().filter(Course.id == id)
+	return qry.fetch()[0]
+########## course fucntions ##################
+
+
+########## extra features ##################
+def gpa_calc():
+	pass
+
+########## extra features ##################
+
+
+
+
+
--- website-jinja/main.py
+++ website-jinja/main.py
@@ -18,11 +18,11 @@ import webapp2
 import jinja2
 import os
 import sys
-sys.path.append(r'C:\Users\Angelica Yunshu Li\Documents\HERE\Projects\course_website\website-jinja\templates\py')
-import User as u
+#sys.path.append(r'C:\Users\Angelica Yunshu Li\Documents\HERE\Projects\course_website\website-jinja\templates\py')
+from User import *
+import json
 
 FOLDERNAME = "templates" 	# only for self.render(template)   e.g. html files
-
 
 
 template_dir = os.path.join(os.path.dirname(__file__), FOLDERNAME)
@@ -44,39 +44,87 @@ class Handler(webapp2.RequestHandler):
 	def set_ck(self,cookie):
 		self.response.headers.add_header('Set-Cookie',cookie)
 
+	def get_ck(self,cookie):
+		return self.request.cookies.get(cookie)
+
+	def load(self,page,user):
+		courseinfo = (json.loads(user.courseinfo) if user.courseinfo else {})
+		courses=(json.loads(user.courses) if user.courses else [])
+		name=user.name
+		self.render(page,user=user,name=name,courses=courses,courseinfo=courseinfo)
+
 class MainHandler(Handler):
 	def get(self):
-		#self.write(open('/static/index.html').read())
-		self.render('index.html')
+		self.render('signup.html')
 	def post(self):
 		uid = self.request.get('userid')
 		pw = self.request.get('password')
 		if 'login' in self.request.POST:
-			user = u.login(uid,pw)
+			user = login(uid,pw)
 			if user:
+				ck = text_hash(user.userid)
+				self.set_ck(str("id=%s"%ck))
 				#title = "%s's Fall 2016"%user.name
-				self.render('main.html',courses=['CS225','cs173'],title = '')
-		elif 'signup' in self.request.POST:
-			name = self.request.get('name')
-			email = self.request.get('email')
-			if u.query_id(uid):
-				self.write('Username has already been taken.')
-			elif u.query_email(email):
-				self.write('email taken')
+				self.redirect('/%s'%userid)
 			else:
-				user_key = u.signup(name,uid,email,pw)
+				self.render('signup.html',loginerror='Invalid username and/or password.',loginactive=True)
+		elif 'signup' in self.request.POST:
+			name = self.request.get('name').capitalize()
+			email = self.request.get('email')
+			if query_id(uid):
+				self.render('signup.html',signuperror='This username has already been taken.')
+			elif query_email(email):
+				self.render('signup.html',signuperror='This email has already been registered.')
+			else:
+				user_key = signup(name,uid,email,pw)
 				if user_key:
-					self.write(user_key)
-					self.set_ck('key=%s'%user_key)
-					self.write('signed up')
+					user = user_key.get()
+					ck = text_hash(user.userid)
+					self.set_ck(str("id=%s"%ck))
+					userurl = '/'+user.userid
+					self.redirect('/%s'%uid)
 		else:
-			self.redirect('/')
-		
+			self.write("???")
 
+class UserHandler(Handler):
+	def get(self,userid):
+		user = query_id(userid)[0]
+		if user.courses:
+			self.load('main.html', user)
+		else:
+			self.redirect('/%s/settings'%userid)
+
+
+
+class SettingsHandler(Handler):
+	def get(self,userid):
+		user = query_id(userid)[0]
+		self.load('setup.html',user= user)
+	def post(self,userid):
+		user = query_id(userid)[0]
+		if 'setup' in self.request.POST:		# new user (first time setup)
+			# update user.courses
+			user.courses = json.dumps(self.request.get('course-list').split())
+			# update user.courseinfo
+			courseinfo = {}
+			for course in user.courses:
+				courseinfo[course] = {}
+			user.courseinfo = json.dumps(courseinfo)
+			# commit updated user
+			user.put()
+			# reload page
+			self.load('setup.html', user = user)
+		elif 'update' in self.request.POST:		# not first time
+			# @TODO: add update function
+			self.write("update request")
+
+		else:
+			self.write("nothing?")
+			
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), webapp2.Route('/<userid>',UserHandler),webapp2.Route('/<userid>/settings',SettingsHandler)
 
 ], debug=True)
