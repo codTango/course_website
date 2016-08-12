5b327780fa81143a55264ab28b819cb960392a38
angelicali <liyunshuangelica@gmail.com>
2016-08-12 13:58:28 +0800

    add course button working

--- website-jinja/templates/js/settings.js
+++ website-jinja/templates/js/settings.js
@@ -1,37 +1,85 @@
 /**
  * Created by Angelica Yunshu Li on 8/2/2016.
  */
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+
+
+// add new course function
+var new_course_id = 0;
+var default_info_list = ['Course Website','Homework','Calendar','Syllabus'];
+function new_course() {
+    var new_course = '<table class="clear" align="center"> \
+        <tr> \
+            <td></td> \
+            <td class="title">Course ID</td> \
+            <td align="left" class="url"><input name="course{0}" placeholder="e.g. CS125"></td> \
+        </tr>'.format(new_course_id);
+    for(i=0; i<default_info_list.length;i++) {
+        var title = default_info_list[i];
+        new_course += '<tr> \
+                            <td class="delete btn" align="right"><img src="/images/delete.png" class="icon"/></td> \
+                            <td class="title">{0}<input type="hidden" name="btn{1}-{2}" value="{0}"></td> \
+                            <td align="left" class="url"><input name="btn_url{1}-{2}" class="url" placeholder="{0} URL" /></td> \
+                        </tr>'.format(default_info_list[i],new_course_id,i);
+    };
+    new_course += '<tr> \
+                    <td class="add btn" align="right"><img src="/images/add.png" class="icon"/></td> \
+                    </tr>';
+    new_course_id+=1;
+    return new_course;
+};
+
+
+
+
+
+
+
 $(document).ready(function(){
 
 
     /* add url row */
-    var newrow = "<tr> \
+    var new_row = "<tr> \
                     <td class='delete btn' align='right'><img src='/images/delete.png' class='icon'/></td>\
                     <td class='title'><input name='new-btn'></td>\
                     <td align='left' class='url'><input class='url' name='new-btn-url' text='{{courseinfo.get(course).get(name)}}'/></td>\
                     </tr>";
-    $(".add").on("click",function(){
+    $("section").on("click","td.add",function(){
         var row = $(this).parent();
-        $(row).before(newrow);
+        $(row).before(new_row);
     });
 
     /* detele url row */
-    $("table").on("click","td.delete",function(){
+    $("section").on("click","td.delete",function(){
         var row = $(this).parent();
         $(row).fadeOut().remove();
     });
-    $("table").on("mouseenter","td.delete",function(){
+    $("section").on("mouseenter","td.delete",function(){
         $(this).find("img").remove();
        $(this).prepend("<span>Delete</span>");
     });
-    $("table").on("mouseleave","td.delete",function(){
+    $("section").on("mouseleave","td.delete",function(){
         $(this).find("span").remove();
         $(this).append("<img src='/images/delete.png' class='icon'/>");
     });
 
     /*TODO: edit courseid*/
     /*TODO: delete course*/
-    /*TODO: add course*/
+    /* add course*/
+    $("#add-course").click(function(){
+        $(this).after(new_course());
+    })
 
 
 });
