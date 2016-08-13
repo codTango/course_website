fbf887d124aaa3c93d895c474ba5e05a67654b35
angelicali <liyunshuangelica@gmail.com>
2016-08-13 13:08:20 +0800

    comments + TODOs + reformat

--- website-jinja/main.py
+++ website-jinja/main.py
@@ -18,143 +18,150 @@ import webapp2
 import jinja2
 import os
 import sys
-#sys.path.append(r'C:\Users\Angelica Yunshu Li\Documents\HERE\Projects\course_website\website-jinja\templates\py')
+# sys.path.append(r'C:\Users\Angelica Yunshu Li\Documents\HERE\Projects\course_website\website-jinja\templates\py')
 from User import *
 import json
 from webapp2_extras import routes
 
-
-FOLDERNAME = "templates" 	# only for self.render(template)   e.g. html files
-
+FOLDERNAME = "templates"  # only for self.render(template)   e.g. html files
 
 template_dir = os.path.join(os.path.dirname(__file__), FOLDERNAME)
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
-
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 
 class Handler(webapp2.RequestHandler):
-	def write(self, *a, **kw):
-		self.response.out.write(*a,**kw)
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
-	def set_ck(self,cookie):
-		self.response.headers.add_header('Set-Cookie',cookie)
+    def set_ck(self, cookie):
+        self.response.headers.add_header('Set-Cookie', cookie)
 
-	def get_ck(self,cookie):
-		return self.request.cookies.get(cookie)
+    def get_ck(self, cookie):
+        return self.request.cookies.get(cookie)
 
-	def load(self,page,user,active='home'):
-		courses = (json.loads(user.courseinfo) if user.courseinfo else {})
-		name=user.name
-		self.render(page,user=user,name=name,courses=courses,active=active)
+    def load(self, page, user, active='home'):
+        courses = (json.loads(user.courseinfo) if user.courseinfo else {})
+        name = user.name
+        self.render(page, user=user, name=name, courses=courses, active=active)
+
 
 class MainHandler(Handler):
-	def get(self):
-		self.render('signup.html')
-	def post(self):
-		uid = self.request.get('userid')
-		pw = self.request.get('password')
-		if 'login' in self.request.POST:
-			user = login(uid,pw)
-			if user:
-				ck = text_hash(user.userid)
-				self.set_ck(str("id=%s"%ck))
-				#title = "%s's Fall 2016"%user.name
-				self.redirect('/%s'%uid)
-			else:
-				self.render('signup.html',loginerror='Invalid username and/or password.',loginactive=True)
-		elif 'signup' in self.request.POST:
-			name = self.request.get('name').capitalize()
-			email = self.request.get('email')
-			if query_id(uid):
-				self.render('signup.html',signuperror='This username has already been taken.')
-			elif query_email(email):
-				self.render('signup.html',signuperror='This email has already been registered.')
-			else:
-				user_key = signup(name,uid,email,pw)
-				if user_key:
-					user = user_key.get()
-					ck = text_hash(user.userid)
-					self.set_ck(str("id=%s"%ck))
-					userurl = '/'+user.userid
-					self.redirect('/%s'%uid)
-		else:
-			self.write("???")
+    def get(self):
+        self.render('signup.html')
+
+    def post(self):
+        uid = self.request.get('userid')
+        pw = self.request.get('password')
+        if 'login' in self.request.POST:
+            user = login(uid, pw)
+            if user:
+                ck = text_hash(user.userid)
+                self.set_ck(str("id=%s" % ck))
+                # title = "%s's Fall 2016"%user.name
+                self.redirect('/%s' % uid)
+            else:
+                self.render('signup.html', loginerror='Invalid username and/or password.', loginactive=True)
+        elif 'signup' in self.request.POST:
+            name = self.request.get('name').capitalize()
+            email = self.request.get('email')
+            if query_id(uid):
+                self.render('signup.html', signuperror='This username has already been taken.')
+            elif query_email(email):
+                self.render('signup.html', signuperror='This email has already been registered.')
+            else:
+                user_key = signup(name, uid, email, pw)
+                if user_key:
+                    user = user_key.get()
+                    ck = text_hash(user.userid)
+                    self.set_ck(str("id=%s" % ck))
+                    userurl = '/' + user.userid
+                    self.redirect('/%s' % uid)
+        else:
+            self.write("???")
+
 
 class UserHandler(Handler):
-	def get(self,userid):
-		user = query_id(userid)[0]
-		if user.courseinfo:
-			self.load('main.html', user)
-		else:
-			self.redirect('/%s/settings'%userid)
+    def get(self, userid):
+        user = query_id(userid)[0]
+        if user.courseinfo:
+            self.load('main.html', user)
+        else:
+            self.redirect('/%s/settings' % userid)
+
 
 class CourseHandler(Handler):
-	def get(self,userid,courseid):
-		user = query_id(userid)[0]
-		if user.courseinfo:
-			self.load('main.html', user,active=courseid)
-		else:
-			self.redirect('/%s/settings'%userid)
+    def get(self, userid, courseid):
+        user = query_id(userid)[0]
+        if user.courseinfo:
+            self.load('main.html', user, active=courseid)
+        else:
+            self.redirect('/%s/settings' % userid)
+
 
 class SettingsHandler(Handler):
-	def get(self,userid):
-		user = query_id(userid)[0]
-		self.load('setup.html',user= user,active="settings")
-	def post(self,userid):
-		user = query_id(userid)[0]
-		if 'update' in self.request.POST:
-			args = self.request.arguments()
-			total = len(filter(lambda x:"course" in x, args))
-			btn_args = filter(lambda x: "btn" in x and "url" not in x, args)
-			courseinfo = {}
-			for i in range(total):
-				courseID = self.request.get("course%d"%i)
-				if courseID:
-					btns = {}
-					btn_lst = filter(lambda x: "btn%d"%i in x, btn_args)
-					for btn in btn_lst:
-						btn_name = self.request.get(btn)
-						btn_url = self.request.get(btn.replace("btn","btn_url"))
+    def get(self, userid):
+        user = query_id(userid)[0]
+        self.load('setup.html', user=user, active="settings")
 
-						if btn_name and btn_url:
-							btns[btn_name]=btn_url
-					courseinfo[courseID]=btns
-			user.courseinfo = json.dumps(courseinfo)
-			user.put()
-			self.redirect("/%s"%userid)
-		elif 'upload' in self.request.POST:
-			img = str(self.request.get("scheduleImage"))
-			user.schedule = img
-			user.put()
-			self.redirect('/img?img_id=%s'%user.key.urlsafe())
-		else:
-			self.write("nothing?")
+    def post(self, userid):
+        user = query_id(userid)[0]
+        if 'update' in self.request.POST:       # update courseinfo
+            args = self.request.arguments()
+            total = len(filter(lambda x: "course" in x, args))
+            btn_args = filter(lambda x: "btn" in x and "url" not in x, args)
+            courseinfo = {}
+            for i in range(total):
+                courseID = self.request.get("course%d" % i)
+                if courseID:
+                    btns = {}
+                    btn_lst = filter(lambda x: "btn%d" % i in x, btn_args)
+                    for btn in btn_lst:
+                        btn_name = self.request.get(btn)
+                        btn_url = self.request.get(btn.replace("btn", "btn_url"))
+
+                        if btn_name and btn_url:
+                            btns[btn_name] = btn_url
+                    courseinfo[courseID] = btns
+            user.courseinfo = json.dumps(courseinfo)
+            user.put()
+            self.redirect("/%s" % userid)
+        elif 'upload' in self.request.POST:         # upload schedule picture
+            img = str(self.request.get("scheduleImage"))
+            user.schedule = img
+            user.put()
+            self.redirect('/img?img_id=%s' % user.key.urlsafe())
+        else:
+            self.write("nothing?")
 
 
 class SubdomainHandler(Handler):
-	def get(self,subdomain):
-		self.write("my spot yay!\n")
-		self.write(subdomain)
+    def get(self, subdomain):
+        self.write("my spot yay!\n")
+        self.write(subdomain)
 
+# Handles image fetch
 class Image(webapp2.RequestHandler):
-	def get(self):
-		user_key = ndb.Key(urlsafe=self.request.get('img_id'))
-		user = user_key.get()
-		if user.schedule:
-			self.response.headers['Content-Type'] = 'image/png'
-			self.response.out.write(user.schedule)
-		else:
-			self.response.out.write('No image')
+    def get(self):
+        user_key = ndb.Key(urlsafe=self.request.get('img_id'))
+        user = user_key.get()
+        if user.schedule:
+            self.response.headers['Content-Type'] = 'image/png'
+            self.response.out.write(user.schedule)
+        else:
+            self.response.out.write('No image')
 
 
 app = webapp2.WSGIApplication([
-routes.DomainRoute('<userid>.kiwi-ninja.appspot.com',[webapp2.Route('/',handler=UserHandler),webapp2.Route('/settings',handler=SettingsHandler),webapp2.Route('/<courseid>',CourseHandler)]),
-	('/', MainHandler), ('/img',Image),webapp2.Route('/<userid>',UserHandler),webapp2.Route('/<userid>/settings',SettingsHandler),webapp2.Route('/<userid>/<courseid>',CourseHandler)
+    routes.DomainRoute('<userid>.kiwi-ninja.appspot.com',
+                       [webapp2.Route('/', handler=UserHandler), webapp2.Route('/settings', handler=SettingsHandler),
+                        webapp2.Route('/<courseid>', CourseHandler)]),
+    ('/', MainHandler), ('/img', Image), webapp2.Route('/<userid>', UserHandler),
+    webapp2.Route('/<userid>/settings', SettingsHandler), webapp2.Route('/<userid>/<courseid>', CourseHandler)
 ], debug=True)
--- website-jinja/templates/js/settings.js
+++ website-jinja/templates/js/settings.js
@@ -2,35 +2,30 @@
  * Created by Angelica Yunshu Li on 8/2/2016.
  */
 if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
 }
 
 
-
-
-
-$(document).ready(function(){
-    if (document)
-
+$(document).ready(function () {
 
     /* add url row */
-    $("section").on("click","td.add",function(){
+    $("section").on("click", "td.add", function () {
         var row = $(this).parent();
         var last_btn = $(row).prev().children(":last").children();
-        if(last_btn.length>0) {
+        if (last_btn.length > 0) {
             var last_btn_number = last_btn.attr("name").slice(7).split("-");
             var new_btn_number = last_btn_number[0] + "-" + (last_btn_number[1] + 1);
-        }else{
+        } else {
             var course_number = $(row).parent().parent().prev().children(":last").attr("name").slice(6);
-            var new_btn_number = course_number+"-"+0;
+            var new_btn_number = course_number + "-" + 0;
         }
         var new_row = "<tr> \
                     <td class='delete btn' align='right'><img src='/images/delete.png' class='icon'/></td>\
@@ -45,21 +40,21 @@ $(document).ready(function(){
     });
 
     /* detele url row */
-    $("section").on("click","td.delete",function(){
+    $("section").on("click", "td.delete", function () {
         var row = $(this).parent();
         $(row).fadeOut().remove();
     });
-    $("section").on("mouseenter","td.delete",function(){
+    $("section").on("mouseenter", "td.delete", function () {
         $(this).find("img").remove();
-       $(this).prepend("<span>Delete</span>");
+        $(this).prepend("<span>Delete</span>");
     });
-    $("section").on("mouseleave","td.delete",function(){
+    $("section").on("mouseleave", "td.delete", function () {
         $(this).find("span").remove();
         $(this).append("<img src='/images/delete.png' class='icon'/>");
     });
 
     /*TODO: edit courseid*/
-    $("section").on("click","img.edit",function(){
+    $("section").on("click", "img.edit", function () {
         var input = $(this).parent().next();
         var input_value = $(input).attr("value");
         var input_name = $(input).attr("name");
@@ -71,14 +66,14 @@ $(document).ready(function(){
                         <td></td> \
                         <td class="title">Course ID</td> \
                         <td align="left" class="url"><input name="{0}" value="{1}"></td> \
-                    </tr>'.format(input_name,input_value));
+                    </tr>'.format(input_name, input_value));
         $(h2).remove();
 
 
     });
 
     /*delete course*/
-    $("section").on("click","img.delete",function(){
+    $("section").on("click", "img.delete", function () {
         var div = $(this).parent().parent().parent();
         $(div).fadeOut().remove();
     });
@@ -87,7 +82,8 @@ $(document).ready(function(){
     /* add course*/
     // add new course function
     var new_course_id = document.getElementsByTagName("table").length;
-    var default_info_list = ['Course Website','Homework','Calendar','Syllabus','Grades'];
+    var default_info_list = ['Course Website', 'Homework', 'Calendar', 'Syllabus', 'Grades'];
+
     function new_course() {
         var new_course = '<table class="clear" align="center"> \
             <tr> \
@@ -95,23 +91,24 @@ $(document).ready(function(){
                 <td class="title">Course ID</td> \
                 <td align="left" class="url"><input class="new-course" name="course{0}" placeholder="e.g. CS125"></td> \
             </tr>'.format(new_course_id);
-        for(i=0; i<default_info_list.length;i++) {
+        for (i = 0; i < default_info_list.length; i++) {
             var title = default_info_list[i];
             new_course += '<tr> \
                                 <input type="hidden" name="bid{1}-{2}" value="{2}">\
                                 <td class="delete btn" align="right"><img src="/images/delete.png" class="icon"/></td> \
                                 <td class="title">{0}<input type="hidden" name="btn{1}-{2}" value="{0}"></td> \
                                 <td align="left" class="url"><input name="btn_url{1}-{2}" class="url" placeholder="{0} URL" /></td> \
-                            </tr>'.format(default_info_list[i],new_course_id,i);
-        };
+                            </tr>'.format(default_info_list[i], new_course_id, i);
+        }
+        ;
         new_course += '<tr> \
                         <td class="add btn" align="right"><img src="/images/add.png" class="icon"/></td> \
                         </tr>';
-        new_course_id+=1;
+        new_course_id += 1;
         return new_course;
     };
 
-    $("#add-course").click(function(){
+    $("#add-course").click(function () {
         $(this).after(new_course());
         var focusbox = document.getElementsByClassName("new-course")[0];
         focusbox.focus();
@@ -127,12 +124,11 @@ $(document).ready(function(){
                 $('#schedule').attr('src', e.target.result);
                 $('#schedule').show();
             }
-
             reader.readAsDataURL(input.files[0]);
         }
     }
 
-    $("#imgInp").change(function(){
+    $("#imgInp").change(function () {
         readURL(this);
     });
 
